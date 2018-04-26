43bbb7d39255dee756f93129550a419946e960b4
gajera154 <31563936+gajera154@users.noreply.github.com>
2018-04-26 17:23:00 +0530

    Add files via upload

---
+++ B-MasterLink.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(10, 11);
+
+int state = 0;
+const int ledPin = 8;  //the pin your led is connected to
+const int buttonPin = 2;  //the pin your button is connected to
+
+int buttonState = 0;
+
+void setup() {
+
+  Serial.begin(9600);
+  BTserial.begin(38400);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+
+}
+
+void loop() {
+
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+    delay(2000);
+    BTserial.write('1');  //sends a 1 through the bluetooth serial link
+    delay (2000);
+  }
+
+  else {
+    BTserial.write('0');
+    digitalWrite(ledPin, LOW);
+    delay(2000);
+  }
+}
---
+++ B_SlaveLink.ino
@@ -0,0 +1,30 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(10, 11);
+
+int state = 0;
+
+void setup() {
+  // initialize digital pin 8 as an output.
+
+  Serial.begin(9600);
+  BTserial.begin(38400);
+
+  pinMode(8, OUTPUT);
+
+}
+
+void loop() {
+
+  if (BTserial.available() > 0) { // Checks whether data is comming from the serial port
+    state = BTserial.read(); // Reads the data from the serial port
+  }
+  // Controlling the LED
+  if (state == '1') {
+    digitalWrite(8, HIGH); // LED ON
+    state = 0;
+  }
+  else if (state == '0') {
+    digitalWrite(8, LOW); // LED ON
+    state = 0;
+  }
+}
