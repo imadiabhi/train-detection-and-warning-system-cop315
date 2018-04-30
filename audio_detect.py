968b6d10c5cf0946c2b2d1ef4b144dbf29532d29
gajera154 <31563936+gajera154@users.noreply.github.com>
2018-04-30 10:34:50 +0530

    Update audio_detect.py

--- audio_detect.py
+++ audio_detect.py
@@ -1,6 +1,7 @@
 import scipy.io.wavfile as wav
 import numpy as np
 import math
+import os
 
 def mean2(x):
     y = np.sum(x) / np.size(x);
@@ -12,12 +13,14 @@ def corr2(a,b):
     r = (a*b).sum() / math.sqrt((a*a).sum() * (b*b).sum());
     return r
 
-(rate,t37) = wav.read("37.wav")
-(rate,t19) = wav.read("19.wav")
+os.system("arecord -f cd -D plughw:1,0 -d 6 recorded_sound.wav")   #recording
+(rate,t37) = wav.read("37.wav")                                    #storing in a matrix  
+(rate,t) = wav.read("recorded_sound.wav")
+t = t[0:264600]
 t37 = t37/32767.0
-t19 = t19/32767.0
-t37e = abs(np.fft.fft(t37.T))
-t19e = abs(np.fft.fft(t19.T))
-R = corr2(t37e,t19e)
+t = t/32767.0
+t37e = abs(np.fft.fft(t37.T))                                      #taking fast fourier transform
+t = abs(np.fft.fft(t.T))
+R = corr2(t37e,t19e)                                               #computing correlation coefficient
 if(R>0.65):
 	print("Train is Here")
